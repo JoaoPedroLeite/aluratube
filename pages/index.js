63e5b7fa2cfe905661dd3c4e768c16b2646e3a11
Joao Pedro <jplff18@hotmail.com>
2022-11-09 14:00:11 -0300

    Implementacao do dark mode

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,6 @@ function HomePage() {
 
     return (
         <>
-            <CSSReset />
             <div style={{
                 display: "flex",
                 flexDirection: "column",
@@ -45,6 +44,8 @@ export default HomePage
 
 
 const StyledHeader = styled.div`
+    background-color: ${({ theme }) => theme.backgroundLevel1};
+
     img {
         width: 80px;
         height: 80px;
